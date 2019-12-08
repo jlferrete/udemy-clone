f34d3ef422d170ddb7c51c4c1f7ec80620313c83
Jlferrete <joseluisferrete@gmail.com>
2019-12-08 12:20:35 +0100

    Added watch to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,12 @@ function css() {
         .pipe(gulp.dest('css'));
 }
 
+function watchFiles() {
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('index.html');
+}
+
 //Registrar funciones como tareas
 
 gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFiles));
